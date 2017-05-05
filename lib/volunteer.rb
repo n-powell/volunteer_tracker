b3d494326614a5082ae56e3166352ae0aeeb1090
Nick Powell <nickpowell@Nicks-iMac.local>
2017-05-05 10:18:44 -0700

    add passing spec for all method on volunteers.

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -6,4 +6,16 @@ class Volunteer
     @name = attributes.fetch(:name)
     @project_id = attributes.fetch(:project_id)
   end
+
+  define_singleton_method(:all) do
+    returned_volunteers = DB.exec("SELECT * FROM volunteers;")
+    volunteers = []
+    returned_volunteers.each() do |volunteer|
+      name = volunteer.fetch("name")
+      id = volunteer.fetch("id").to_i()
+      project_is = volunteer.fetch("project_id")
+      volunteers.push(Volunteer.new({:id => id, :name => name, :project_id => project_id}))
+    end
+    volunteers
+  end
 end
