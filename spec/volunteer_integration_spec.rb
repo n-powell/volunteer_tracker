9ad15c29c923f35a09b6dbbd65ec02a455273a72
Nick Powell <nickpowell@Nicks-iMac.local>
2017-05-10 22:23:01 -0700

    add integration specs to test specific inputs are saved and indeed displayed in proper locations.

--- spec/volunteer_integration_spec.rb
+++ spec/volunteer_integration_spec.rb
@@ -5,6 +5,15 @@ Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
 describe('the volunteer path', {:type => :feature}) do
+  it ('goes to the index page and confirms there are not yet any projects in projects list.') do
+    visit('/')
+    expect(page).to have_content('There are no projects yet!')
+  end
+
+  it ('goes to the index page and confirms there are not yet any volunteers in the volunteers in projects list.') do
+    visit('/')
+    expect(page).to have_content('There are no volunteers yet!')
+  end
 
   it ('Takes user input project name and adds it to the projects list.') do
     visit('/')
@@ -14,7 +23,7 @@ describe('the volunteer path', {:type => :feature}) do
     expect(page).to have_content('sweeping')
   end
 
-  it ('Takes user input project name and adds it to the projects list.') do
+  it ('tests the update function of the project name.') do
     visit('/')
     fill_in('project_name', :with => 'Trim the trees')
     click_button('Add')
@@ -26,7 +35,7 @@ describe('the volunteer path', {:type => :feature}) do
     expect(page).to have_content('Trim the trees and bushes')
   end
 
-  it ('Takes user input project name and adds it to the projects list.') do
+  it ('tests the delete function of the project name from projects list.') do
     visit('/')
     fill_in('project_name', :with => 'Trim the trees')
     click_button('Add')
@@ -39,15 +48,20 @@ describe('the volunteer path', {:type => :feature}) do
     expect(page).to have_content('There are no projects yet!')
   end
 
-  it ('Takes user input project name and adds it to the projects list.') do
+
+  it ('Takes user volunteer name and adds it to the volunteer list for a specific project. Then tests that the name is displayed in the volunteers currently in projects list') do
     visit('/')
     fill_in('project_name', :with => 'sweep')
     click_button('Add')
     click_link('Return Home')
-    expect(page).to have_content('sweep')
+    click_link('sweep')
+    fill_in('name', :with => 'Kyle')
+    click_button('Add Volunteer')
+    click_link('Return Home')
+    expect(page).to have_content('Kyle')
   end
 
-  it ('Takes user volunteer name and adds it to the volunteer list.') do
+  it ('Takes user volunteer name and adds it to the volunteer list for a specific project.') do
     visit('/')
     fill_in('project_name', :with => 'sweep')
     click_button('Add')
